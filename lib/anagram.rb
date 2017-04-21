1f776d1895ba10a0366186f25c883d2c8c457d8f
Grady L Shelton <gradyish@gmail.com>
2017-04-21 15:16:03 -0700

    refactor word check and repass all tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,7 +3,7 @@ class String
     result = 'antigrams'
     input_1 = self.downcase.delete('^a-z').split('')
     input_2 = str.downcase.delete('^a-z').split('')
-    if self.scan(/^[^aeiouyAEIOUY]+$|^$|(.)\1{2,}/) != [] || str.scan(/^[^aeiouyAEIOUY]+$|^$|(.)\1{2,}/) != []
+    if (self||str).scan(/^[^aeiouyAEIOUY]+$|^$|(.)\1{2,}/) != []
       result = 'not a word'
     else
       if input_1.reverse == input_2
