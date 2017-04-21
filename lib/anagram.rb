c23e65d312861523dc4cd87cac12c922fb60b848
Grady L Shelton <gradyish@gmail.com>
2017-04-21 10:51:48 -0700

    add test that specifically targets partial anagrams and pass. differing string lengths are accounted for as evidenced between tests 4 and 5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,48 @@
 class String
   define_method(:anagram) do |str|
-    # letters = self.downcase.split("")
-    # argument_letters = str.downcase.split("")
     result = false
-    if self.downcase.delete("^a-z").reverse == str.downcase.delete("^a-z")
+    aa = self.downcase.split('').sort
+    bb = str.downcase.split('').sort
+    if self.downcase.delete('^a-z').reverse == str.downcase.delete('^a-z')
       result = 'palindrome'
-    elsif self.downcase.split('').sort == str.downcase.split('').sort
-        result = true
+    elsif aa == bb
+      result = 'anagram'
+    elsif aa.each do |aletter|
+# binding.pry
+      if bb.any? {|bletter| bletter == aletter }
+        result = 'partial anagram!'
+      end
+    end
     end
     result
   end
 end
+
+# elsif array1.each do |aletter|
+#   if array2.any?(aletter)
+#     result = 'partial anagram!'
+#   else
+#     result = "antigram"
+#   end
+# end
+
+# class String
+#   define_method(:anagram) do |str|
+#     letters = self.downcase.split("")
+#     argument_letters = str.downcase.split("")
+#     result = false
+#     if self.downcase.delete("^a-z").reverse == str.downcase.delete("^a-z")
+#       result = 'palindrome'
+#     elsif letters.each do |aletter|
+#       argument_letters.each_with_index do |bletter, index|
+#         if bletter.include?(aletter)
+#           result = true
+#         else
+#           result = false
+#         end
+#       end
+#     end
+#     end
+#     result
+#   end
+# end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,22 +4,26 @@ require('pry')
 
 describe('String#anagram') do
   it('check if two inputs are anagrams') do
-    expect('ruby'.anagram('bury')).to(eq(true))
+    expect('ruby'.anagram('bury')).to(eq('anagram'))
   end
 
   it('check if two inputs are anagrams, despite case differences') do
-    expect('Ruby'.anagram('BurY')).to(eq(true))
+    expect('Ruby'.anagram('BurY')).to(eq('anagram'))
   end
 
   it('check if two inputs are anagrams, and also if they are palindromes') do
     expect('leper'.anagram('repel')).to(eq('palindrome'))
   end
 
-  it('check if two inputs are anagrams, and also if they are palindromes') do
-    expect('lepers'.anagram('repel')).to(eq(false))
+  it('check if two inputs are asymmetric palindromes between the two inputs are false and also check that partial matches are false as anagrams') do
+    expect('lepers'.anagram('repel')).to(eq('partial anagram!'))
   end
 
-  # it('check if two inputs are antigrams') do
-  #   expect('hi'.anagram('bye')).to(eq('antigram'))
+  it('check if two inputs are partial anagrams') do
+    expect('hello'.anagram('hebbo')).to(eq('partial anagram!'))
+  end
+  #
+  # it('check if two inputs are partial anagrams') do
+  #   expect('hello'.anagram('he')).to(eq('partial anagram!'))
   # end
 end
