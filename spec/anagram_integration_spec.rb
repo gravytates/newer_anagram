cdf1f605bf35dac4be1dff36b638e5a9be5f531e
Grady L Shelton <gradyish@gmail.com>
2017-04-21 14:09:53 -0700

    pass fifth integration test showing if non-words are detected

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -31,12 +31,21 @@ describe('the anagram path', {:type => :feature}) do
     expect(page).to have_content('partial anagrams')
   end
 
-  it('processes user entries and evaluates two words as partial anagrams') do
+  it('processes user entries and evaluates two words as palindromes') do
     visit('/')
     fill_in('input_one', :with => 'Leper')
     fill_in('input_two', :with => 'repel')
 
     click_button('Anagrize!')
     expect(page).to have_content('palindromes')
+  end
+
+  it('processes user entries and evaluates if a non-word is detected') do
+    visit('/')
+    fill_in('input_one', :with => 'zkh')
+    fill_in('input_two', :with => 'zkh')
+
+    click_button('Anagrize!')
+    expect(page).to have_content('not a word')
   end
 end
