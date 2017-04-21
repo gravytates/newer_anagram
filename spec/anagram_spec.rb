e2dd5ee4ebd8e0361c2571d96385ec6f707b8042
Grady L Shelton <gradyish@gmail.com>
2017-04-21 09:19:44 -0700

    pass test checking for anagrams despite case differences

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,4 +6,8 @@ describe('String#anagram') do
   it('check if two inputs are anagrams') do
     expect('ruby'.anagram('bury')).to(eq(true))
   end
+
+  it('check if two inputs are anagrams, despite case differences') do
+    expect('Ruby'.anagram('BurY')).to(eq(true))
+  end
 end
