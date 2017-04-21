0df2f1b0f339d84656c05adc3585f98ab0c640df
Grady L Shelton <gradyish@gmail.com>
2017-04-21 09:33:57 -0700

    pass third test to also check if two inputs are palindromes in addition to being anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,12 +3,15 @@ class String
     letters = self.downcase.split("")
     argument_letters = str.downcase.split("")
     result = false
-    letters.each do |aletter|
+    if self.downcase.delete("^a-z").reverse == str.downcase.delete("^a-z")
+      result = 'palindrome'
+    elsif letters.each do |aletter|
       argument_letters.each_with_index do |bletter, index|
         if bletter.include?(aletter)
           result = true
         end
       end
+    end
     end
     result
   end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -10,4 +10,8 @@ describe('String#anagram') do
   it('check if two inputs are anagrams, despite case differences') do
     expect('Ruby'.anagram('BurY')).to(eq(true))
   end
+
+  it('check if two inputs are anagrams, and also if they are palindromes') do
+    expect('leper'.anagram('repel')).to(eq('palindrome'))
+  end
 end
