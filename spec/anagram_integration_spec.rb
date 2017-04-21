f3f63a365084e9321d2f05ca767fdf8d800a6dc8
Grady L Shelton <gradyish@gmail.com>
2017-04-21 14:08:14 -0700

    pass fourth integration test showing two words with different capitalization evaluating to palindromes

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -30,4 +30,13 @@ describe('the anagram path', {:type => :feature}) do
     click_button('Anagrize!')
     expect(page).to have_content('partial anagrams')
   end
+
+  it('processes user entries and evaluates two words as partial anagrams') do
+    visit('/')
+    fill_in('input_one', :with => 'Leper')
+    fill_in('input_two', :with => 'repel')
+
+    click_button('Anagrize!')
+    expect(page).to have_content('palindromes')
+  end
 end
