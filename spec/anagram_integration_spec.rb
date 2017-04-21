4df010fa67ebb8bbab60c347f8b05074f7e08193
Grady L Shelton <gradyish@gmail.com>
2017-04-21 14:05:43 -0700

    pass second integration test showing two words evaluating to antigrams

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -4,12 +4,21 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the anagram path', {:type => :feature}) do
-  it('processes user entries and evaluates whether strings are anagrams, partial anagrams, antigrams or palindromes') do
+  it('processes user entries and evaluates two words as anagrams') do
     visit('/')
     fill_in('input_one', :with => 'leep')
     fill_in('input_two', :with => 'pele')
 
     click_button('Anagrize!')
     expect(page).to have_content('anagrams')
+  end
+
+  it('processes user entries and evaluates two words as antigrams') do
+    visit('/')
+    fill_in('input_one', :with => 'leep')
+    fill_in('input_two', :with => 'curt')
+
+    click_button('Anagrize!')
+    expect(page).to have_content('antigrams')
   end
 end
